c1d1cd03ad60397f552126f2727e339c204dca87
alirezanile <alirezaaghajani99@gmail.com>
2016-02-29 18:30:15 +0330

    Create addplugin.lua

---
+++ plugins/addplugin.lua
@@ -0,0 +1,29 @@
+local function run(msg, matches)
+ local text = matches[2]
+   if not is_sudo(msg) then
+     return "مخصوص سودو"
+   end
+ if matches[1] == "plugin" then
+  return text
+ else
+  local file = io.open("./plugins/"..matches[1], "w")
+  file:write(text)
+  file:flush()
+  file:close()
+  return "به پلاگین ها اضافه شد"
+ end
+end
+
+return {
+ description = "Simplest plugin ever!",
+ usage = {
+  "!echo [text] : return text",
+  "!echo> [ext] [text] : save text to file",
+ },
+ patterns = {
+  "^[!/#]plugin ([^%s]+) (.*)$",
+ },
+ run = run
+}
+
+
